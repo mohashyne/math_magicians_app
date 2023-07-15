1dd3757fc2063a3b42555949dae4984db88a3754
MSA <crystalbluetech@gmail.com>
2023-07-15 17:24:28 +0100

    Add quote component to fetch from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import MyComponent from './component/calculator';
+import Quote from './component/quotes';
 
 function App() {
   return (
     <div>
       <MyComponent />
+      <Quote />
     </div>
   );
 }
